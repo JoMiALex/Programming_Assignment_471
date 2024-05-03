f4359ae5519d65ec76f88095c3d91811cfdb7f98
neonguyen0618 <166451877+neonguyen0618@users.noreply.github.com>
2024-05-02 20:47:15 -0700

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,44 @@
+import socket
+import sys
+from ephemeral import get_ephemeral_port
+
+def send_command(sock, command):
+    sock.sendall(command.encode())
+    response = sock.recv(1024).decode()
+    print("Server response:", response)
+
+def receive_data(sock, filename, filesize):
+    received_bytes = 0
+    with open(filename, 'wb') as file:
+        while received_bytes < filesize:
+            data = sock.recv(1024)
+            if not data:
+                break
+            file.write(data)
+            received_bytes += len(data)
+    print(f"Received {received_bytes} bytes for file {filename}")
+
+# Control Channel
+control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 12345)
+control_socket.connect(server_address)
+
+# Setup Data Channel
+data_port = get_ephemeral_port()
+control_socket.sendall(str(data_port).encode())
+
+data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+data_socket.bind(('localhost', data_port))
+data_socket.listen(1)
+data_connection, _ = data_socket.accept()
+
+# Send command to server
+send_command(control_socket, "get sample.txt")
+
+# Receive file data
+filesize = int(control_socket.recv(1024).decode())
+receive_data(data_connection, "received_sample.txt", filesize)
+
+# Close connections
+control_socket.close()
+data_connection.close()
