1f1028de80c0b3807965ddc567e2eca2bc4cd7b2
neonguyen0618 <166451877+neonguyen0618@users.noreply.github.com>
2024-05-02 20:48:05 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,3 +1,10 @@
+#python server side TCP
+#
+#@author John Michael Lott
+#@author Mariah Salgado
+#@author Tuan Nguyen
+#@author Ibrahim Israr
+
 import socket
 import sys
 from ephemeral import get_ephemeral_port
