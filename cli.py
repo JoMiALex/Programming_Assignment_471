c7d71886a64eaca100029538b175196ccf2d81ff
JoMiAlex <lottjohnmichael@yahoo.com>
2024-05-03 22:54:02 -0700

    up

--- cli.py
+++ cli.py
@@ -7,17 +7,22 @@
 
 import socket
 import sys
-from ephemeral import get_ephemeral_port
+import os
 
 def send_command(sock, command):
+    
     sock.sendall(command.encode())
     response = sock.recv(1024).decode()
     print("Server response:", response)
+    return response
 
-def receive_data(sock, filename, filesize):
+def receive_file(sock, filename):
+    # Receives a file from the server
+    file_size_str = sock.recv(10).decode()
+    file_size = int(file_size_str.strip())
     received_bytes = 0
     with open(filename, 'wb') as file:
-        while received_bytes < filesize:
+        while received_bytes < file_size:
             data = sock.recv(1024)
             if not data:
                 break
@@ -43,10 +48,8 @@ def ftp_client_loop(sock):
         dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
         print ("Data socket successfully created")
         
-        dataSock.bind(('localhost', 0))         
+        dataSock.bind(('localhost', 45123))         
         print ("Data socket binded to port:", dataSock.getsockname()[1])
-
-        sock.sendall(dataSock.getsockname()[1].encode())
 
         dataSock.listen(1)     
         print ("Data socket is listening")
@@ -76,7 +79,7 @@ def ftp_client_loop(sock):
 
 print("Starting FTP client...")
 control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 12345)
+server_address = ('localhost', 45321)
 try:
     control_socket.connect(server_address)
     print("Connected to the server.")
@@ -89,4 +92,3 @@ ftp_client_loop(control_socket)
 
 print("Closing connections...")
 control_socket.close()
-data_connection.close()
